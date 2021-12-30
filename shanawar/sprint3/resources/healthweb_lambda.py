5a4c96c44a8bc8a843766705053bc9430b6c6ba1
Ubuntu <ubuntu@ip-172-31-41-54.us-east-2.compute.internal>
2021-12-30 15:47:10 +0000

    updated Whlambda

--- shanawar/sprint3/resources/healthweb_lambda.py
+++ shanawar/sprint3/resources/healthweb_lambda.py
@@ -1,39 +1,46 @@
 import urllib3,datetime
 import constants as constants
 from cloudwatch_putdata import CloudWatch_PutMetric
+from resources.bucket import s3bucket 
 
 def lambda_handler(events,context):
     values= dict()
     cw= CloudWatch_PutMetric()
-    
-    avail= get_availability()
-    dimensions=[
-        {'Name':'URL','Value': constants.URL_to_Monitor},
-    ]
-    cw.put_data(constants.URL_Monitor_Namespace,constants.URL_Monitor_Name_Availability,dimensions,avail)
-    
-    
-    latency= get_latency()
-    dimensions=[
-        {'Name':'URL','Value': constants.URL_to_Monitor},
-    ]
-    cw.put_data(constants.URL_Monitor_Namespace,constants.URL_Monitor_Name_Latency,dimensions,latency)
-    
-    values.update({"Availability": avail,"Latency":latency})
+    s3_bucket = s3bucket()
+    #s3_bucket.store_urls('shanawarbucket')
+    URLS = s3_bucket.get_bucket('shanawarbucket')
+    count=1
+    for url in URLS:
+        avail= get_availability(url)
+        dimensions=[
+            {'Name':'URL','Value': constants.URL_to_Monitor},
+        ]
+        cw.put_data(constants.URL_Monitor_Namespace,constants.URL_Monitor_Name_Availability+'  '+url+ str(count),dimensions,avail)
+        
+        
+        latency= get_latency(url)
+        dimensions=[
+            {'Name':'URL','Value': constants.URL_to_Monitor},
+        ]
+        cw.put_data(constants.URL_Monitor_Namespace,constants.URL_Monitor_Name_Latency+'  '+url+ str(count),dimensions,latency)
+        
+        values.update({"Availability": avail,"Latency":latency})
+        count+=1
     return values
+        
 
-def get_availability():
+def get_availability(url):
     http=urllib3.PoolManager()
-    response=http.request("GET",constants.URL_to_Monitor)
+    response=http.request("GET",url)
     if response.status==200:
         return 1.0
     else:
         return 0.0
     
-def get_latency():
+def get_latency(url):
     http=urllib3.PoolManager()
     start=datetime.datetime.now()
-    response=http.request("GET",constants.URL_to_Monitor)
+    response=http.request("GET",url)
     end=datetime.datetime.now()
     diff=end-start
     latency_sec=round(diff.microseconds * 0.000001,6)
