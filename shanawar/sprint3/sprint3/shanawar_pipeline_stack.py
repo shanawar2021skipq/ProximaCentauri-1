5583554f7476051a5b42c3c2208273eb9c9cd062
Ubuntu <ubuntu@ip-172-31-41-54.us-east-2.compute.internal>
2022-01-03 20:19:06 +0000

    p

--- shanawar/sprint3/sprint3/shanawar_pipeline_stack.py
+++ shanawar/sprint3/sprint3/shanawar_pipeline_stack.py
@@ -45,15 +45,15 @@ class PipelineStack(core.Stack):
             role=pipelineroles,
             role_policy_statements=[iamPolicy,stsPolicy]
             )
-            
+        """
         integration_test = pipelines.CodeBuildStep(
             'integration_tests',input=source,
             commands=["cd shanawar/sprint3","pip install -r requirements.txt", "npm install -g aws-cdk", "pytest integration_tests"],
             role=pipelineroles,
             role_policy_statements=[iamPolicy,stsPolicy]
             )
-
-        pipeline.add_stage(beta, pre=[unit_test],post=[integration_test])
+        """
+        pipeline.add_stage(beta, pre=[unit_test])
     ################# STEP4: PROD ###################    
         pipeline.add_stage(prod,pre=[pipelines.ManualApprovalStep("Go to Production")])
         
